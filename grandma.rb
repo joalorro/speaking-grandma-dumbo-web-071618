20b670d097b0d2c8ba1f19d4e56a26975f76be30
Josue V. Alorro <joalorro@gmail.com>
2018-06-28 15:37:16 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,11 @@ def speak_to_grandma
   
   if greeting == 'I LOVE YOU GRANDMA!'
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif greeting != greeting.upcase
+  elsif greeting == greeting.downcase
     puts "HUH!? SPEAK UP, SONNY!"
     puts "HUH!? SPEAK UP, SONNY!"
-  elsif greeting == greeting.
+  else
+    puts 'NO, NOT SINCE 1938!'
+    puts 'NO, NOT SINCE 1938!'
+  end
+end
