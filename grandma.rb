b988fbf742ce80055227c41c6e2887dac49b350e
Josue V. Alorro <joalorro@gmail.com>
2018-06-28 15:39:38 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -18,9 +18,9 @@ def speak_to_grandma(greeting)
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif greeting != greeting.downcase
     puts "HUH!? SPEAK UP, SONNY!"
-    puts "HUH!? SPEAK UP, SONNY!"
+    return "HUH!? SPEAK UP, SONNY!"
   else
     puts 'NO, NOT SINCE 1938!'
-    puts 'NO, NOT SINCE 1938!'
+    return 'NO, NOT SINCE 1938!'
   end
 end
